30f10cedc01c5f507ec0eba623c568d962dfbb30
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-07-23 18:19:50 -0400

    App charts

--- App/app.py
+++ App/app.py
@@ -1,12 +1,20 @@
 import streamlit as st 
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+
+pio.renderers.default = "notebook"
+
 
 st.markdown("""
 # Hello
+Double Click for Original Zoom
 """)
 
 
 df = px.data.gapminder().query("country=='Canada'")
-fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+df
 
-st.plotly_chart(fig)
+fig0 = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+st.plotly_chart(fig0)
+st.write('yes Yes')
