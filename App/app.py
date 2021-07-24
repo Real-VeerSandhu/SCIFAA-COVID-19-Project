4c9f0a7c285169c9146dc61d230338d9e7a7bc9f
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-07-24 03:12:12 -0400

    App graphs

--- App/app.py
+++ App/app.py
@@ -1,20 +1,23 @@
 import streamlit as st 
 import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
+import pandas as pd
 
-pio.renderers.default = "notebook"
 
 
 st.markdown("""
-# Hello
+# Canda v.s. the US COVID-19 Analysis
 Double Click for Original Zoom
 """)
+st.write('Comparing how metrics on how the countries dealt with the pandemic and measurment the damages caused')
 
 
-df = px.data.gapminder().query("country=='Canada'")
-df
+combined = pd.read_csv('Data/out-data.csv')
 
-fig0 = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+fig0 = px.line(combined, x='Date', y='Cases/Population', color='Country', template = "plotly_dark", title = "Cases per Person in the USA vs Canada", labels={'Cases/Population': 'Cases per Person'})
+fig1 = px.line(combined, x='Date', y='Deaths/Cases', color='Country', template = "plotly_dark", title = "Deaths per Case in the USA vs Canada", labels={'Deaths/Cases': 'Deaths per Case'})
+fig2 = px.line(combined, x='Date', y='Tests/Cases', color='Country', template = "plotly_dark", title = "Tests per Case in the USA vs Canada", labels={'Tests/Cases': 'Tests per Case'})
+
 st.plotly_chart(fig0)
-st.write('yes Yes')
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
+
